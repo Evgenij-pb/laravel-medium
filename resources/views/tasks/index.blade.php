ab0a2de628acd2c246edae88e02d70ca96d56516
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-11-11 17:07:57 +0200

    view for all tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <a href="{{route('task.create')}}" class="btn btn-success"><i class="fa fa-plus"></i>Create new task</a>
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+                            <td>
+                                <form action="{{ route('task.destroy', $task->id) }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fa fa-btn fa-trash"></i>Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+@endsection
